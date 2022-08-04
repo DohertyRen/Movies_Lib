2f27889a8193bd38a28dbd8d3a72037b1d9a05d3
DohertyRen <dohertyrenato@gmail.com>
2022-08-04 12:37:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./index.css"
+import { Outlet } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+
+function App() {
+ 
+  return (
+    <div className="App">
+    <Navbar />
+    <Outlet />
+    </div>
+  )
+}
+
+export default App
